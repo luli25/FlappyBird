108b4dfd37562ac39f5a28505e3c359178fa49e2
LucilaOliver <lucilaoliver8@gmail.com>
2026-04-28 17:34:46 -0300

    Added UI and restart game feature.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,18 @@
 using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject gameOverText;
+    
     public static GameManager Instance;
     public bool IsGameOver { get; private set; }
     
     public event Action OnGameOver;
+
+    private bool _canRestart = false;
 
     private void Awake()
     {
@@ -21,9 +27,30 @@ public class GameManager : MonoBehaviour
 
     public void GameOver()
     {
-        if (IsGameOver) return;
+        if (IsGameOver)
+        {
+            return;
+        }
 
         IsGameOver = true;
+        gameOverText.SetActive(true);
         OnGameOver?.Invoke();
+        
+        Invoke(nameof(EnableRestart), 0.5f); // Adds a small delay before restarting the scene
+    }
+
+    private void EnableRestart()
+    {
+        _canRestart = true;
+    }
+
+    public void Restart()
+    {
+        if (!_canRestart)
+        {
+            return;
+        }
+        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -18,30 +18,24 @@ public class Player : MonoBehaviour
     private void OnEnable()
     {
         InputManager.Instance.OnFlap += Flap;
-        GameManager.Instance.OnGameOver += HandleGameOver;
     }
 
     private void OnDisable()
     {
         InputManager.Instance.OnFlap -= Flap;
-        GameManager.Instance.OnGameOver -= HandleGameOver;
     }
 
     private void Flap()
     {
         if (GameManager.Instance.IsGameOver)
         {
+            GameManager.Instance.Restart();
             return;
         }
         
         _playerRb.linearVelocity = Vector2.zero;
         _playerRb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         _anim.SetTrigger("Flap");
-    }
-    
-    private void HandleGameOver()
-    {
-        InputManager.Instance.OnFlap -= Flap;
     }
 
     private void OnCollisionEnter2D()
