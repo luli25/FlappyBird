c10a689a61b2bf6749a890a38a91697a6a0361e4
LucilaOliver <lucilaoliver8@gmail.com>
2026-04-30 15:34:44 -0300

    Added coin prefab.

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -1,10 +1,21 @@
-using System;
 using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
-    private void OnTriggerEnter2D()
+    [SerializeField] private ParticleSystem burstFx;
+    [SerializeField] private AudioSource coinSound;
+    private void OnTriggerEnter2D(Collider2D other)
     {
+        if (burstFx != null)
+        {
+            burstFx.transform.SetParent(null);
+            burstFx.Play();
+            Destroy(burstFx.gameObject, burstFx.main.duration
+                                        + burstFx.main.startLifetime.constantMax);
+        }
+        
+        AudioSource.PlayClipAtPoint(coinSound.clip, transform.position);
+        
         GameManager.Instance.IncreaseCoinCount();
         Destroy(gameObject);
     }
